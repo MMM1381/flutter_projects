99dcb67ea579b32d485dc473dd4fa8491f082d68
MohammadMehdi Moazedi <mehdimoazedy@gmail.com>
2024-10-01 16:25:59 +0330

    list and loop

---
+++ dart/list.dart
@@ -0,0 +1,11 @@
+void hello(String name) {
+  print("hello, $name");
+}
+
+void main() {
+  List<String> greetings = ["mehdi", "nadi", "alireza"];
+
+  for (var name in greetings) {
+    hello(name);
+  }
+}
